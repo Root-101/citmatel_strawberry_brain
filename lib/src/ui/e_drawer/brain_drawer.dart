4ec126f2d14bf65ca1eebb4a1b1ca1cf2b50e327
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-08 17:55:26 -0500

    BrainDrawer: clear

--- lib/src/ui/e_drawer/brain_drawer.dart
+++ lib/src/ui/e_drawer/brain_drawer.dart
@@ -4,51 +4,8 @@ import 'package:get/get.dart';
 class BrainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 120,
-            width: double.infinity,
-            padding: EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
-            color: Theme.of(context).accentColor,
-            child: Text(
-              'Playing!!!',
-              style: TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 30,
-                  color: Theme.of(context).primaryColor),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          _buildListTile('Ligth', Icons.light_mode_outlined, () {
-            Get.changeTheme(ThemeData.light());
-          }),
-          _buildListTile('Dark', Icons.dark_mode_outlined, () {
-            Get.changeTheme(ThemeData.dark());
-          }),
-          _buildListTile(
-            'About',
-            Icons.local_police,
-            () {
-              showAboutDialog(
-                context: context,
-                applicationVersion: 'version number',
-                applicationIcon: Icon(Icons.ac_unit),
-                applicationLegalese: "Hellouuuuuu",
-                children: [
-                  Text('hi 1'),
-                  Text('hi 2'),
-                  Text('hi 3'),
-                ],
-              );
-            },
-          ),
-        ],
-      ),
+    return Container(
+      color: Colors.purple,
     );
   }
 
