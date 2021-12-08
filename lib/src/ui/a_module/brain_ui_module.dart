61372ee0da7afd6fd9db3475d8d10707f34a719d
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-08 17:54:58 -0500

    BrainUIModule: +Get.put => BrainZoomDrawerController

--- lib/src/ui/a_module/brain_ui_module.dart
+++ lib/src/ui/a_module/brain_ui_module.dart
@@ -1,8 +1,11 @@
-
+import 'package:citmatel_strawberry_brain/src/ui/brain_ui_exporter.dart';
 import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
+import 'package:get/get.dart';
 
 class BrainUIModule {
   static void init() {
     TriviaUIModule.init();
+
+    Get.put<BrainZoomDrawerController>(BrainZoomDrawerControllerImpl());
   }
 }
