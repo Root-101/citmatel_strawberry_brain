45bd97d16b257f264c415cca5b61611639c79601
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-21 16:12:51 -0500

    +ui_constants

---
+++ lib/src/ui/ui_constants.dart
@@ -0,0 +1,5 @@
+const String APP_ICON_URL = "assets/app_icon.png";
+const String APP_SPLASH_URL = "assets/app_splash_initial.png";
+
+const String APP_NAME = "Strawberry Brain";
+const String VERSION_NUMBER = "0.1.0";
