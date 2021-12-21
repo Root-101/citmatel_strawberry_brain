9766e373f1e02e458db3e717589b3dedbdcacc17
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-21 11:32:50 -0500

    main.dart: +SystemChrome.setPreferredOrientations only portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:citmatel_strawberry_brain/src/ui/brain_ui_exporter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   BrainUIModule.init();
@@ -10,6 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //aqui y no en el main por un posible bug en flutter, info at https://stackoverflow.com/questions/49418332/flutter-how-to-prevent-device-orientation-changes-and-force-portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return GetMaterialApp(
       title: 'Playing App',
       debugShowCheckedModeBanner: false,
