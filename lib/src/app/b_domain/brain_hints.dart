93bf6e6fcce0c57b752688f82154ac5d770ebb85
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-28 22:45:48 -0500

    BrainHints: +dummy data

--- lib/src/app/b_domain/brain_hints.dart
+++ lib/src/app/b_domain/brain_hints.dart
@@ -3,5 +3,9 @@ import 'package:citmatel_strawberry_brain/src/app/b_domain/brain_domain_exporter
 class BrainHints {
   static final hints = [
     BrainHintDomain(id: 1, hint: "hihihi"),
+    BrainHintDomain(id: 2, hint: "123\n456\n789"),
+    BrainHintDomain(id: 3, hint: "hello world!!!"),
+    BrainHintDomain(id: 4, hint: "Show me\nthe\nmoneeeeeeeeeeeey"),
+    BrainHintDomain.list(id: 5, hintParts: ["hola", "mundo", "cruel"]),
   ];
 }
