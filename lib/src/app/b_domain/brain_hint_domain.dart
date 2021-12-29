e4e4dd52c03c6194395e9ed7176ae2fe455c08b5
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-28 22:45:20 -0500

    BrainHintDomain: +factory BrainHintDomain.list

--- lib/src/app/b_domain/brain_hint_domain.dart
+++ lib/src/app/b_domain/brain_hint_domain.dart
@@ -2,10 +2,17 @@ import 'package:clean_core/clean_core.dart';
 
 class BrainHintDomain extends BasicDomainObject {
   int id;
-  final String hint;
+  late final String hint;
 
   BrainHintDomain({
     required this.id,
     required this.hint,
   });
+
+  BrainHintDomain.list({
+    required this.id,
+    required List<String> hintParts,
+  }) {
+    this.hint = hintParts.reduce((value, element) => "$value\n$element");
+  }
 }
