5fd2d87b6248efefaf6f8d8d0502640d1aa66394
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-28 23:48:35 -0500

    +SingleSplashModel

---
+++ lib/src/ui/f_widget/secondary_splashscreen/single_splash_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SingleSplashModel {
+  final Widget splash;
+  final int duration;
+
+  SingleSplashModel({required this.splash, this.duration = 2});
+}
