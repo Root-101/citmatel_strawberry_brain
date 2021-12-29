5663416af951c61feecf6575fd44080916995276
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-28 22:46:43 -0500

    BrainHintUseCase & Impl: +nextHint

--- lib/src/app/d_usecase_impl/brain_hint_usecase_impl.dart
+++ lib/src/app/d_usecase_impl/brain_hint_usecase_impl.dart
@@ -1,7 +1,16 @@
+import 'dart:math';
+
 import 'package:citmatel_strawberry_brain/src/app/brain_app_exporter.dart';
 import 'package:clean_core/clean_core.dart';
 
 class BrainHintUseCaseImpl extends DefaultReadUseCase<BrainHintDomain>
     implements BrainHintUseCase {
   BrainHintUseCaseImpl(List<BrainHintDomain> info) : super(info);
+
+  @override
+  BrainHintDomain nextHint() {
+    return findAll()[Random().nextInt(
+      count(),
+    )];
+  }
 }
