5e3cffeae17838340d27545712a6b5e3077ece89
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-28 22:47:06 -0500

    BrainUIModule: init() => +BrainCoreModule.init()

--- lib/src/ui/a_module/brain_ui_module.dart
+++ lib/src/ui/a_module/brain_ui_module.dart
@@ -1,10 +1,12 @@
-import 'package:citmatel_strawberry_brain/src/ui/brain_ui_exporter.dart';
+import 'package:citmatel_strawberry_brain/brain_exporter.dart';
 import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
 import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
 import 'package:get/get.dart';
 
 class BrainUIModule {
   static void init() {
+    BrainCoreModule.init();
+
     TriviaUIModule.init();
     HangManUIModule.init();
 
