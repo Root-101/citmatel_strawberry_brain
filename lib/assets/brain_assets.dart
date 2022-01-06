461b528871098f64af8a549cb533b210cc92bae1
JHB <jhernandezb96@gmail.com>
2022-01-05 22:17:02 -0500

    +BrainAssets.dart

---
+++ lib/assets/brain_assets.dart
@@ -0,0 +1,7 @@
+class BrainAssets {
+  static const _assets = "packages/citmatel_strawberry_brain/assets";
+  static const _character = "$_assets/character";
+
+  static const String CHARACTER_FIELD = "$_character/field.jpg";
+  static const String CHARACTER_PRESENTATION = "$_character/presentation.jpg";
+}
