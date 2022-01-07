c100b45e55628123c385082d94aa8d9653a2db75
JHB <jhernandezb96@gmail.com>
2022-01-06 23:18:06 -0500

    Fix logo client & dev

--- lib/assets/brain_assets.dart
+++ lib/assets/brain_assets.dart
@@ -1,7 +1,11 @@
 class BrainAssets {
   static const _assets = "packages/citmatel_strawberry_brain/assets";
   static const _character = "$_assets/character";
+  static const _app = "$_assets/app";
 
-  static const String CHARACTER_FIELD = "$_character/field.jpg";
-  static const String CHARACTER_BACKGROUND = "$_character/background.jpg";
+  static const String CHARACTER_FIELD = "$_character/background_dashboard.jpg";
+  static const String CHARACTER_BACKGROUND = "$_character/background_main.jpg";
+
+  static const String APP_CLIENT_LOGO = "$_app/client_logo.png";
+  static const String APP_DEV_LOGO_FLAT = "$_app/dev_logo_flat.png";
 }
