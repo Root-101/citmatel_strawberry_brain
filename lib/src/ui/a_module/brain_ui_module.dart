111e404b9e91a04f03825f7533eb0e605c830d3a
JHB <jhernandezb96@gmail.com>
2022-01-08 17:24:32 -0500

    +RandomController & Impl

--- lib/src/ui/a_module/brain_ui_module.dart
+++ lib/src/ui/a_module/brain_ui_module.dart
@@ -1,7 +1,7 @@
 import 'package:citmatel_strawberry_brain/brain_exporter.dart';
-import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
-import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
 import 'package:citmatel_strawberry_dnd/dnd_exporter.dart';
+import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
+import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
 import 'package:get/get.dart';
 
 class BrainUIModule {
@@ -13,5 +13,6 @@ class BrainUIModule {
     DnDUIModule.init();
 
     Get.put<BrainZoomDrawerController>(BrainZoomDrawerControllerImpl());
+    Get.put<BrainRandomController>(BrainRandomControllerImpl());
   }
 }
