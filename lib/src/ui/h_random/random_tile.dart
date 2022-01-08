c9c67882266abde246c942eee9b6cd9143d1aeea
JHB <jhernandezb96@gmail.com>
2022-01-08 17:24:58 -0500

    +Random Tile

---
+++ lib/src/ui/h_random/random_tile.dart
@@ -0,0 +1,72 @@
+import 'package:animations/animations.dart';
+import 'package:citmatel_strawberry_brain/brain_exporter.dart';
+import 'package:citmatel_strawberry_dnd/dnd_exporter.dart';
+import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
+import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class RandomTile extends StatelessWidget {
+  late Widget _random;
+
+  RandomTile({Key? key}) : super(key: key) {
+    _random = _randomSubLevelWidget();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: OpenContainer(
+        onClosed: (_) {
+          _random = _randomSubLevelWidget();
+        },
+        closedBuilder: (_, __) {
+          return Container(
+            color: Colors.purple,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.approval,
+                  color: Colors.black,
+                ),
+                Text(
+                  "Aleatorio",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        openBuilder: (_, __) {
+          return _random;
+        },
+      ),
+    );
+  }
+
+  _randomSubLevelWidget() {
+    BrainRandomController controller = Get.find();
+
+    GameMode mode = controller.randomGameMode();
+    switch (mode) {
+      case GameMode.TRIVIA:
+        return TriviaSubLevelBackground(
+          subLevelDomain: controller.randomTriviaSublevel(),
+        );
+      case GameMode.HANGMAN:
+        return HangManSubLevelBackground(
+          subLevelDomain: controller.randomHangmanSublevel(),
+        );
+      case GameMode.DND:
+        return DnDSubLevelBackground(
+          subLevelDomain: controller.randomDnDSublevel(),
+        );
+    }
+  }
+}
---
+++ lib/src/ui/h_random/ui_random_exporter.dart
@@ -0,0 +1,4 @@
+library ui_random_exporter;
+
+//export all the widgets
+export 'random_tile.dart';
