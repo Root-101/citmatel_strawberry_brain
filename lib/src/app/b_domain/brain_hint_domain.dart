1d1ce40ab5798466378701807843c800c7c3e2fa
JHB <jhernandezb96@gmail.com>
2022-01-09 02:48:46 -0500

    BrainHintDomain: +clone

--- lib/src/app/b_domain/brain_hint_domain.dart
+++ lib/src/app/b_domain/brain_hint_domain.dart
@@ -1,6 +1,6 @@
 import 'package:clean_core/clean_core.dart';
 
-class BrainHintDomain extends BasicDomainObject {
+class BrainHintDomain extends BasicDomainObject<BrainHintDomain> {
   int id;
   late final String hint;
 
@@ -14,5 +14,13 @@ class BrainHintDomain extends BasicDomainObject {
     required List<String> hintParts,
   }) {
     this.hint = hintParts.reduce((value, element) => "$value\n$element");
+  }
+
+  @override
+  BrainHintDomain clone() {
+    return BrainHintDomain(
+      id: this.id,
+      hint: this.hint,
+    );
   }
 }
