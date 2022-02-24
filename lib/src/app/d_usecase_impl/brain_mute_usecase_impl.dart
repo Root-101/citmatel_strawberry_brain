f15650588f5d5f68bb7d5ec4521adae16830ded4
JHB <jhernandezb96@gmail.com>
2022-02-24 14:34:22 -0500

    BrainMuteController: typo

--- lib/src/app/d_usecase_impl/brain_mute_usecase_impl.dart
+++ lib/src/app/d_usecase_impl/brain_mute_usecase_impl.dart
@@ -1,12 +1,12 @@
 import 'package:citmatel_strawberry_brain/brain_exporter.dart';
 import 'package:clean_repo_objectbox/clean_objectbox_exporter.dart';
 
-class BoolKeyValueUseCaseImpl extends SingleKeyValueUseCaseImpl<String, bool>
+class BrainMuteUseCaseImpl extends SingleKeyValueUseCaseImpl<String, bool>
     implements BrainMuteUseCase {
   static const String KEY = "MUTE_KEY";
   static const bool DEFAULT_VALUE = false;
 
-  BoolKeyValueUseCaseImpl()
+  BrainMuteUseCaseImpl()
       : super(
           key: KEY,
           defaultValue: DEFAULT_VALUE,
