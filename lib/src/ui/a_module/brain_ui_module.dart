eed301d1fcb02bbabf5eb746a73f589574a044c7
JHB <jhernandezb96@gmail.com>
2022-04-06 22:34:13 -0400

    Modules all in

--- lib/src/ui/a_module/brain_ui_module.dart
+++ lib/src/ui/a_module/brain_ui_module.dart
@@ -1,13 +1,15 @@
 import 'package:citmatel_strawberry_brain/brain_exporter.dart';
 import 'package:citmatel_strawberry_dnd/dnd_exporter.dart';
+import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
+import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
 import 'package:get/get.dart';
 
 class BrainUIModule {
   static Future init() async {
     await BrainCoreModule.init();
 
-    // await TriviaUIModule.init();
-    // await HangManUIModule.init();
+    await TriviaUIModule.init();
+    await HangManUIModule.init();
     await DnDUIModule.init();
 
     Get.put<BrainZoomDrawerController>(BrainZoomDrawerControllerImpl());
